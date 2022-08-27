ba5deadeb8fb42378bb66b446b6d92abb396ffa0
fmo <mustafa.ozyurt@gmail.com>
2022-08-27 20:37:28 +0200

    Initial Commit

---
+++ src/main/java/com/fmo/fmanager/FmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.fmo.fmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fmo/fmanager/FmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmo.fmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
