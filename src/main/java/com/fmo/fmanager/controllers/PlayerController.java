c57eb237859aceb0e7027ebbfe9e572e3592d5a2
fmo <mustafa.ozyurt@gmail.com>
2022-08-28 10:56:58 +0200

    Player controller

---
+++ src/main/java/com/fmo/fmanager/controllers/PlayerController.java
@@ -0,0 +1,12 @@
+package com.fmo.fmanager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class PlayerController {
+    @GetMapping("addPlayerPage")
+    public String addPlayerPage(Model model) {
+        return "index";
+    }
+}
