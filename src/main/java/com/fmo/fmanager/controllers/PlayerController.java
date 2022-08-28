3c68429ec4cffe9dbf292f8ac3e779dd557fa0db
fmo <mustafa.ozyurt@gmail.com>
2022-08-28 14:35:51 +0200

    Positions

--- src/main/java/com/fmo/fmanager/controllers/PlayerController.java
+++ src/main/java/com/fmo/fmanager/controllers/PlayerController.java
@@ -4,12 +4,19 @@ import com.fmo.fmanager.domain.Player;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class PlayerController {
     @GetMapping("addPlayerPage")
     public String addPlayerPage(Model model) {
         Player player = new Player();
         model.addAttribute("player", player);
+
+        List<String> listPosition = Arrays.asList("GK", "DC", "DR", "DL", "DMC", "MC", "FC");
+        model.addAttribute("listPosition", listPosition);
 
         return "add-player-page";
     }
