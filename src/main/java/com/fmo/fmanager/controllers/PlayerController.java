3aa41088b8cb0db73833b3cb86e9d0da298ee547
fmo <mustafa.ozyurt@gmail.com>
2022-08-28 14:05:17 +0200

    Tie the object to player

--- src/main/java/com/fmo/fmanager/controllers/PlayerController.java
+++ src/main/java/com/fmo/fmanager/controllers/PlayerController.java
@@ -1,5 +1,6 @@
 package com.fmo.fmanager.controllers;
 
+import com.fmo.fmanager.domain.Player;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PlayerController {
     @GetMapping("addPlayerPage")
     public String addPlayerPage(Model model) {
+        Player player = new Player();
+        model.addAttribute("player", player);
+
         return "add-player-page";
     }
 }
