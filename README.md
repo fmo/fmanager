e057a6e81d6f3f4c5771a43552a0be555ffbc95b
fmo <mustafa.ozyurt@gmail.com>
2022-09-21 16:04:26 +0200

    Run things on docker

--- README.md
+++ README.md
@@ -5,3 +5,36 @@ kubectl create deployment fm-node --image=fmoz/fmanager:first
 kubectl expose deployment fm-node --type=NodePort --port=8080
 
 minikube service fm-node --url
+
+
+<hr />
+
+## Run everything on Docker
+
+docker network create rabbits
+
+#### To run rabbit container
+docker run -d --rm --net rabbits -p 8081:15672 --hostname rabbit-1 --name rabbit-1 rabbitmq:3.8
+
+#### Rabbit management
+docker exec -it rabbit-1 sh
+
+<li>rabbitmq-plugins enable rabbitmq_management</li>
+
+<li>rabbitmqctl stop</li>
+
+<li>rabbitmqctl start</li>
+
+
+#### Enable stats
+
+cd  /etc/rabbitmq/conf.d/
+
+echo management_agent.disable_metrics_collector = false > management_agent.disable_metrics_collector.conf 
+
+
+#### Run the app
+
+docker build . -t fmanager
+
+docker run -d --rm --net rabbits -p 8090:8080 --name fm fmanager 
---
+++ src/main/java/com/fmo/fmanager/RabbitmqConfig.java
@@ -0,0 +1,43 @@
+package com.fmo.fmanager;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+    private static final String MY_QUEUE = "MyQueue";
+
+    @Bean
+    Queue myQueue() {
+        return new Queue(MY_QUEUE, true);
+    }
+
+    @Bean
+    Binding binding() {
+        return BindingBuilder
+                .bind(myQueue())
+                .to(myExchange())
+                .with("topic")
+                .noargs();
+    }
+
+    @Bean
+    Exchange myExchange() {
+        return ExchangeBuilder.topicExchange("MyTopicExchange")
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cashingConnectionFactory = new CachingConnectionFactory("rabbit-1");
+        cashingConnectionFactory.setUsername("guest");
+        cashingConnectionFactory.setPassword("guest");
+
+        return cashingConnectionFactory;
+    }
+}
